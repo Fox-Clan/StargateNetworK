dca90fa2bc2916d438ea225783a6d99639988d66
gup <guppu525@gmail.com>
2025-02-12 09:24:12 -0800

    assembly info

--- StargateNetwork/obj/Debug/net9.0/StargateNetwork.AssemblyInfo.cs
+++ StargateNetwork/obj/Debug/net9.0/StargateNetwork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StargateNetwork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bcd97797d40fba863e7a0e5c2d423f02364bf18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d99e9ae708601cd5af5f251a0ec2524a2786b92")]
 [assembly: System.Reflection.AssemblyProductAttribute("StargateNetwork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StargateNetwork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
